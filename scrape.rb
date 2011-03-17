13283f5972ebc17b8562e3cb8e5182e3db5148f9
jonathanb <jxb6065@rit.edu>
2011-03-17 17:58:13 -0400

    setup bundler. added inline testing with rspec (stubbed in for now)

--- scrape.rb
+++ scrape.rb
@@ -1,3 +1,11 @@
+require "rubygems"
+require "bundler/setup"
+Bundler.require(:default)
+
 File.open("sample_data") do |f|
   # parse the data
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Bundler.require(:test)
 end
