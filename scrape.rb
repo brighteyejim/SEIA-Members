6678f6ad8804d710af87269e9beacf2814d73ff7
jonathanb <jxb6065@rit.edu>
2011-03-17 18:44:55 -0400

    clarifier test language a little

--- scrape.rb
+++ scrape.rb
@@ -68,25 +68,25 @@ if __FILE__ == $PROGRAM_NAME
       @entry = Scraper.parse_entry(file)
     end
     
-    it "reads in the first entry's company name" do
+    it "reads in a sample entry's company name" do
       @entry[:name].should == "Advanced Solar Industries, LLC"
     end
-    it "reads in the first entry's website" do
+    it "reads in a sample entry's website" do
       @entry[:url].should == "www.advancedsolarindustries.com"
     end
-    it "reads in the first entry's contact" do
+    it "reads in a sample entry's contact" do
       @entry[:contact].should == "Elam Beiler"
     end
-    it "reads in the first entry's address line 1" do
+    it "reads in a sample entry's address line 1" do
       @entry[:adr_1].should == "3530 W. Newport Road"
     end
-    it "reads in the first entry's address line 2" do
+    it "reads in a sample entry's address line 2" do
       @entry[:adr_2].should == "Ronks, PA 17542"
     end
-    it "reads in the first entry's phone number" do
+    it "reads in a sample entry's phone number" do
       @entry[:phone].should == "717-768-8500"
     end
-    it "reads in the first entry's description" do
+    it "reads in a sample entry's description" do
       @entry[:desc].should == "Advanced Solar Industries is an installer of grid-tie systems primarily utilizing SunPower brand solar panels. We also have over 15 years of experience installing off-grid solar systems."
     end
   end
