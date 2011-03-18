a9b85b098459edb6d7afb3dbe365bb2b162df4e9
jonathanb <jxb6065@rit.edu>
2011-03-18 01:59:31 -0400

    added LICENSE, README. fixed an issue with comma that wasn't loading me output to a CSV file. now parses a single page of entries

--- Gemfile
+++ Gemfile
@@ -1,6 +1,12 @@
 source "http://rubygems.org"
 
+#automated WWW interaction + parsing
 gem "mechanize"
+gem "i18n"
+gem "activesupport", :require => "active_support"
+
+#CSV generator DSL
+gem "comma", :git => "git@github.com:jonathanb/comma.git"
 
 group :test do
 	gem "rspec"
--- scrape.rb
+++ scrape.rb
@@ -2,9 +2,31 @@ require "rubygems"
 require "bundler/setup"
 Bundler.require(:default)
 
+# hash wrapper class for the CSV file DSL called "comma"
+class Entry
+  attr_accessor :company, :website, :name, :addr1, :addr2, :addr_rest, :phone, :description
+  
+  comma do
+    company
+    website
+    name
+    addr1
+    addr2
+    addr_rest
+    phone
+    description
+  end
+end
+
 class Scraper
-  BUS_TYPES = ["Contractor/Installer","Distributor","Financial Company or Financial Consultant","Law Firm","Research Laboratory","Manufacturer/Supplier","Utility","Project Developer (Architects, planners, consultants, and builders of solar projects)","Other (non-financial)","Commercial System User","Solar Winery/Brewery/Distillery"]
-  STATES = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+  # form field names
+  FIELDNAME_NAME     = "filter_equal.member_type.name"
+  FIELDNAME_STATE    = "filter_equal.dm_seia_organization.address.state"
+  FIELDNAME_KEYWORDS = "filter_like.dm_seia_organization.description"
+  
+  # form field vals
+  FIELDVALS_BUS_TYPES = ["Contractor/Installer","Distributor","Financial Company or Financial Consultant","Law Firm","Research Laboratory","Manufacturer/Supplier","Utility","Project Developer (Architects, planners, consultants, and builders of solar projects)","Other (non-financial)","Commercial System User","Solar Winery/Brewery/Distillery"]
+  FIELDVALS_STATES    = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
   
   SEIA_URL = "http://www.seia.org/cs/membership/member_directory"
   
@@ -18,36 +40,45 @@ class Scraper
     
     # set form values
     form = page.form("search")
-    form["filter_equal.member_type.name"] = 'Contractor/Installer'
-    form["filter_equal.dm_seia_organization.address.state"] = 'PA'
-    form["filter_like.dm_seia_organization.description"] = ''
+    form[FIELDNAME_NAME]     = 'Contractor/Installer'
+    form[FIELDNAME_STATE]    = 'PA'
+    form[FIELDNAME_KEYWORDS] = ''
     
     # submit form, get results page
     result_page = agent.submit(form, form.buttons.first)
     
     # scrape entries from results page
     entries = []
-    e = {}
+    e = Entry.new
     result_page.search('.results p').each do |p|
       className = p.attr('class')
+
+      break if className.nil?
+      
+      #make friendly for Ruby method names
+      className.gsub!('-', '_')
       
       # 1st/last in sequence for this entry?
       is_first = className == 'company'
       is_last  = className == 'description'
       
-      # puts "#{p.attr('class')} => #{p.text}"
+      #new entry if we just started
+      e = Entry.new if is_first          
       
-      e = {} if is_first        #new entry if we just started
-      e[className] = p.text.chomp #get next tag in sequence
-      entries << e if is_last   #push the entry if we're done
+      #get next tag in sequence
+      clean_txt = Scraper.sanitize_str(p.text)
+      e.send "#{className}=", clean_txt
+      
+      #push the entry if we're done
+      entries << e if is_last  
     end
     
-    # output each entry
-    entries.each_with_index do |e, i|
-      e.each_pair do |k, v|
-        puts "#{k} => #{v}"
-      end
-    end
+    # output to CSV file
+    entries.to_comma(:filename => "OUTPUT.csv")
+  end
+  
+  def self.sanitize_str(str)
+    str.gsub("\r", '').gsub("\n", '').squeeze(' ').strip
   end
   
   # phone number regex (src: http://blog.stevenlevithan.com/archives/validate-phone-number#r4-2-v-inline)
@@ -56,6 +87,7 @@ class Scraper
   end
 end
 
+# RSpec tests
 if __FILE__ == $PROGRAM_NAME
   Scraper.scrape_page
   
